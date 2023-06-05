bf56206aa3d6c4a26cbdcc2d748f2f173531ac5d
Daniil Melehin <d.melehin07@gmail.com>
2023-06-05 21:19:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication 05.06/ConsoleApplication 05.06.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>//перегрузка операторов
+#include "Linkedlist.h"
+
+
+//
+//class Digit
+//{
+//private:
+//    int _data;
+//public:
+//    Digit(int data) : _data(data){ }
+//
+//          int operator +(Digit &digit)
+//    {
+//        return _data + digit._data;
+//
+//    }
+//
+//    //int Foo(Digit& digit) {
+//    //    return _data + digit._data
+//
+//    int operator -(Digit& digit)
+//    {
+//        if (_data < digit._data)
+//            return digit._data - _data;
+//        return _data - digit._data;
+//
+//
+//    }
+//
+//};
+
+
+
+
+
+
+    int main() {
+        setlocale(LC_ALL, "Russian");
+
+        //Digit digit(5);
+        //Digit digit2(10);
+        //std::cout << digit.operator+(digit2) << '\n';
+        //std::cout << digit.operator-(digit2) << '\n';
+        //std::cout << digit + 5 << '\n';
+        //std::cout << digit2 - digit2 << '\n';
+        LinkedList list;
+
+        list.Push_back(109);
+        list.Push_back(80);
+        list.Push_back(40);
+        list.Push_back(55);
+        list.Push_back(66);
+        list.Push_back(85);
+        list.Push_back(45);
+        list.Push_back(25);
+        
+  /* std::cout << list.SearchByIndex(3)->GetData();*/
+        list.PushByIndex(0, 30);
+        list.ShowList();
+
+
+    return 0;
+
+};
---
+++ ConsoleApplication 05.06/LinkedList.cpp
@@ -0,0 +1,62 @@
+#include "Linkedlist.h"
+#include <iostream>
+
+LinkedList::LinkedList()
+{
+	_head = nullptr;
+	_tail = nullptr;
+	_numberOfElements = 0;
+
+}
+
+void LinkedList::Push_back(int data) {
+	Node* newNode = new Node(data);
+	_numberOfElements++;
+
+	if (_head == nullptr)
+	{
+		_head = newNode;
+		_tail = newNode;
+		return;
+	}
+
+	newNode->SetNext(_tail);
+
+	_tail = newNode;
+
+
+}
+
+void LinkedList::ShowList()
+{
+	for (Node* ptr = _tail; ptr != _head->GetNext(); ptr = ptr->GetNext())
+		std::cout << ptr->GetData() << '\n';
+
+
+}
+
+Node* LinkedList::SearchByIndex(int index)
+{
+	Node* ptr = _tail;
+	int cout = _numberOfElements - index;
+	while (cout > 0)
+	{
+		ptr = ptr->GetNext();
+		cout--;
+	}
+	return ptr;
+
+
+}	
+
+void LinkedList::PushByIndex(int data, int index)
+{
+	Node* newNode = new Node(data);
+	_numberOfElements++;
+	Node* elementInIndex = SearchByIndex(index);
+	Node* element = SearchByIndex(index + 1);
+	element->SetNext(newNode);
+	newNode->SetNext(elementInIndex);
+	
+
+}
